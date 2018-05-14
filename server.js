50ab49124172f85be7654ac89c0076a048657b39
Alvin Theodora <alvintheodoraa@gmail.com>
2018-05-14 19:10:57 +0700

    add catch-all protocol for SPA

--- server.js
+++ server.js
@@ -4,4 +4,13 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  });
+
 app.listen(port, ()=>console.log('server on ' + port));
